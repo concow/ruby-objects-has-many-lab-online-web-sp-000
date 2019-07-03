2f83b4e0850d5b1c33157f3bc80aaef00e1dce52
Connor Cowan <ccowan003@comcast.net>
2019-07-03 02:25:34 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -23,7 +23,7 @@ class Author
   def add_post_by_title(title)
     post = Post.new(title)           #creating a new instance of a post
     post.author = self               #Right here all we're doing is associating the objects its that simple
-  end
+  end                                #this method doesnt need to return anything
 
   def self.post_count
     Post.all.count           #class method returning total number of posts associated to all existing authors
